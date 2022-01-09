af9f1c421388d607d07ace85a3f7b358bb35e108
xgill15x <jason_gill15@hotmail.com>
2022-01-08 23:24:52 -0800

    fixing resumeData path

--- website/src/App.js
+++ website/src/App.js
@@ -24,7 +24,7 @@ class App extends Component {
 
   getResumeData(){
     $.ajax({
-      url:'JasonGill/resumeData.json',
+      url:'/resumeData.json',
       dataType:'json',
       cache: false,
       success: function(data){
