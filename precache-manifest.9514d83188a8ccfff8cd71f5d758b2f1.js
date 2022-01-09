c260ba372073ff82ff9fa08a1eba6e37772ddefd
xgill15x <jason_gill15@hotmail.com>
2022-01-09 00:07:09 -0800

    Updates

---
+++ precache-manifest.9514d83188a8ccfff8cd71f5d758b2f1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "631e2dba0e41edd2a94c",
+    "url": "/JasonGill/static/css/main.cc494cf5.chunk.css"
+  },
+  {
+    "revision": "631e2dba0e41edd2a94c",
+    "url": "/JasonGill/static/js/main.c702a294.chunk.js"
+  },
+  {
+    "revision": "5f0d7b29c453c28c99e3",
+    "url": "/JasonGill/static/js/runtime~main.ff7cf165.js"
+  },
+  {
+    "revision": "bac2abfdcd512bda7816",
+    "url": "/JasonGill/static/js/2.8bc79b0b.chunk.js"
+  },
+  {
+    "revision": "5b9ecc46db9c6d0e5b3455cf0567b31a",
+    "url": "/JasonGill/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/JasonGill/precache-manifest.326ebc5a26d5c06696b4f3ce2f212da1.js"
+  "/JasonGill/precache-manifest.9514d83188a8ccfff8cd71f5d758b2f1.js"
 );
 
 workbox.clientsClaim();
