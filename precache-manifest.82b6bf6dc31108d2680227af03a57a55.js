fecc223b700365214a40ca1fae36ad06a864b5de
xgill15x <jason_gill15@hotmail.com>
2022-01-08 22:39:50 -0800

    Updates

---
+++ precache-manifest.82b6bf6dc31108d2680227af03a57a55.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3c44ad51296a710fbd41",
+    "url": "/JasonGill/static/css/main.e4d31c8d.chunk.css"
+  },
+  {
+    "revision": "3c44ad51296a710fbd41",
+    "url": "/JasonGill/static/js/main.c7ea48a4.chunk.js"
+  },
+  {
+    "revision": "5f0d7b29c453c28c99e3",
+    "url": "/JasonGill/static/js/runtime~main.ff7cf165.js"
+  },
+  {
+    "revision": "263acf62ef9c49f50316",
+    "url": "/JasonGill/static/js/2.3b503de2.chunk.js"
+  },
+  {
+    "revision": "4741dc42b24b8abfe36193ca2ad3b82e",
+    "url": "/JasonGill/index.html"
+  }
+];
