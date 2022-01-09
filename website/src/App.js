ce6e5ea348926f3b041cca788bdb6426cd2ed000
xgill15x <jason_gill15@hotmail.com>
2022-01-08 23:35:34 -0800

    updated url

--- website/src/App.js
+++ website/src/App.js
@@ -24,7 +24,7 @@ class App extends Component {
 
   getResumeData(){
     $.ajax({
-      url:'/resumeData.json',
+      url:'https://xgill15x.github.io/JasonGill/resumeData.json',
       dataType:'json',
       cache: false,
       success: function(data){
