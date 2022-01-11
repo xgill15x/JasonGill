df7b6196fad2dd38a45f9a61c307b1700597fdea
xgill15x <jason_gill15@hotmail.com>
2022-01-10 20:58:17 -0800

    resume is downloadable

--- src/Components/About.js
+++ src/Components/About.js
@@ -24,7 +24,7 @@ class About extends Component {
             <div className="row">
                <div className="columns download">
                   <p>
-                     <a href={resumeDownload} className="button"><i className="fa fa-download"></i>Download Resume</a>
+                     <a href='/JasonResume.pdf' className="button" download><i className="fa fa-download"></i>Download Resume</a>
                   </p>
                </div>
             </div>
