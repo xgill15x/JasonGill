2b575b76d274d75c362e39624b4e01cef2304bc7
xgill15x <jason_gill15@hotmail.com>
2023-04-27 14:49:29 -0700

    added work experience

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -1,51 +1,84 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Resume extends Component {
   render() {
+    if (this.props.data) {
+      var education = this.props.data.education.map(function (education) {
+        return (
+          <div key={education.school}>
+            <h3>{education.school}</h3>
+            <p className="info">
+              {education.degree} <span>&bull;</span>
+              <em className="date">{education.graduated}</em>
+            </p>
+            <p>{education.description}</p>
+          </div>
+        );
+      });
 
-    if(this.props.data){
-      var education = this.props.data.education.map(function(education){
-        return <div key={education.school}><h3>{education.school}</h3>
-        <p className="info">{education.degree} <span>&bull;</span><em className="date">{education.graduated}</em></p>
-        <p>{education.description}</p></div>
-      })
-      var projects = this.props.data.projects.map(function(projects){
-        return <div key={projects.title}><h3>{projects.title}</h3>
-            <p className="info">{projects.language}<span>&bull;</span> <a href={projects.link}>{projects.link}</a></p>
+      var work = this.props.data.work.map(function (work) {
+        return (
+          <div key={work.school}>
+            <h3>{work.school}</h3>
+            <p className="info">
+              {work.degree} <span>&bull;</span>
+              <em className="date">{work.graduated}</em>
+            </p>
+            <p>{work.description}</p>
+          </div>
+        );
+      });
+
+      var projects = this.props.data.projects.map(function (projects) {
+        return (
+          <div key={projects.title}>
+            <h3>{projects.title}</h3>
+            <p className="info">
+              {projects.language}
+              <span>&bull;</span> <a href={projects.link}>{projects.link}</a>
+            </p>
             <p>{projects.description}</p>
-        </div>
-      })
+          </div>
+        );
+      });
     }
 
     return (
       <section id="resume">
+        <div className="row work">
+          <div className="three columns header-col">
+            <h1>
+              <span>Work Experience</span>
+            </h1>
+          </div>
 
-      <div className="row work">
+          <div className="nine columns main-col">{work}</div>
+        </div>
 
-      <div className="three columns header-col">
-        <h1><span>Projects</span></h1>
-      </div>
+        <div className="row work">
+          <div className="three columns header-col">
+            <h1>
+              <span>Projects</span>
+            </h1>
+          </div>
 
-      <div className="nine columns main-col">
-      {projects}
-      </div>
-      </div>
+          <div className="nine columns main-col">{projects}</div>
+        </div>
 
-      <div className="row education">
-         <div className="three columns header-col">
-            <h1><span>Education</span></h1>
-         </div>
+        <div className="row education">
+          <div className="three columns header-col">
+            <h1>
+              <span>Education</span>
+            </h1>
+          </div>
 
-         <div className="nine columns main-col">
+          <div className="nine columns main-col">
             <div className="row item">
-               <div className="twelve columns">
-                 {education}
-               </div>
+              <div className="twelve columns">{education}</div>
             </div>
-         </div>
-      </div>
-
-   </section>
+          </div>
+        </div>
+      </section>
     );
   }
 }
