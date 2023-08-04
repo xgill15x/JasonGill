ee2eec2ff290d2c6ac8ce3acaa508f368528e031
xgill15x <jason_gill15@hotmail.com>
2023-08-03 17:03:01 -0700

    update

--- src/App.js
+++ src/App.js
@@ -1,56 +1,54 @@
-import React, { Component } from 'react';
-import ReactGA from 'react-ga';
-import $ from 'jquery';
-import './App.css';
-import Header from './Components/Header';
-import Footer from './Components/Footer';
-import About from './Components/About';
-import Resume from './Components/Resume';
-import Contact from './Components/Contact';
+import React, { Component } from "react";
+import ReactGA from "react-ga";
+import $ from "jquery";
+import "./App.css";
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
+import About from "./Components/About";
+import Resume from "./Components/Resume";
+import Contact from "./Components/Contact";
 
 class App extends Component {
-
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
-      resumeData: {}
+      resumeData: {},
     };
 
-    ReactGA.initialize('UA-110570651-1');
+    ReactGA.initialize("UA-110570651-1");
     ReactGA.pageview(window.location.pathname);
-
   }
 
-  getResumeData(){
-    const mainUrl = 'https://xgill15x.github.io/JasonGill/resumeData.json'
-    const localUrl = '/resumeData.json'
+  getResumeData() {
+    const mainUrl = "https://xgill15x.github.io/JasonGill/resumeData.json";
+    const localUrl = "/resumeData.json";
 
     $.ajax({
-      url:mainUrl,
-      dataType:'json',
+      url: localUrl,
+      dataType: "json",
       cache: false,
-      success: function(data){
-        this.setState({resumeData: data});
+      success: function (data) {
+        this.setState({ resumeData: data });
       }.bind(this),
-      error: function(xhr, status, err){
+      error: function (xhr, status, err) {
         console.log(err);
         alert(err);
-      }
+      },
     });
   }
 
-  componentDidMount(){
+  componentDidMount() {
     this.getResumeData();
   }
 
   render() {
     return (
       <div className="App">
-        <Header data={this.state.resumeData.main}/>
-        <About data={this.state.resumeData.main}/>
-        <Resume data={this.state.resumeData.resume}/>
-        <Contact data={this.state.resumeData.main}/>
-        <Footer data={this.state.resumeData.main}/>
+        <Header data={this.state.resumeData.main} />
+        <About data={this.state.resumeData.main} />
+        <Resume data={this.state.resumeData.resume} />
+        <Contact data={this.state.resumeData.main} />
+        <Footer data={this.state.resumeData.main} />
       </div>
     );
   }
--- src/Components/About.js
+++ src/Components/About.js
@@ -1,40 +1,40 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class About extends Component {
   render() {
-
-    if(this.props.data){
+    if (this.props.data) {
       var name = this.props.data.name;
-      var profilepic= "images/"+this.props.data.image;
+      var profilepic = "images/" + this.props.data.image;
       var bio = this.props.data.bio;
       var email = this.props.data.email;
       var resumeDownload = "/";
     }
 
-    const localUrl = '/JasonGill_Resume.pdf'
-    const mainUrl = 'https://xgill15x.github.io/JasonGill/JasonGill_Resume.pdf'
+    const localUrl = "/JasonGill_Resume.pdf";
+    const mainUrl = "https://xgill15x.github.io/JasonGill/JasonGill_Resume.pdf";
 
     return (
       <section id="about">
-      <div className="row">
-         <div className="three columns">
-            <img className="profile-pic"  src={profilepic} alt="jasonGill" />
-         </div>
-         <div className="nine columns main-col">
+        <div className="row">
+          <div className="three columns">
+            <img className="profile-pic" src={profilepic} alt="jasonGill" />
+          </div>
+          <div className="nine columns main-col">
             <h2>About Me</h2>
 
             <p>{bio}</p>
             <div className="row">
-               <div className="columns download">
-                  <p>
-                     <a href={mainUrl} className="button" download><i className="fa fa-download"></i>Download Resume</a>
-                  </p>
-               </div>
+              <div className="columns download">
+                <p>
+                  <a href={localUrl} className="button" download>
+                    <i className="fa fa-download"></i>Download Resume
+                  </a>
+                </p>
+              </div>
             </div>
-         </div>
-      </div>
-
-   </section>
+          </div>
+        </div>
+      </section>
     );
   }
 }
